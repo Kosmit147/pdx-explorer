ad59ff8a5f392e15d98f459d8552af2a99d0110d
Wojciech Opara <wwojtek660@gmail.com>
2025-09-15 17:32:38 +0200

    Create a basic window and display text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // Hide console window on Windows in release.
+
+use eframe::egui;
+
+#[derive(Default)]
+struct Explorer {}
+
+impl Explorer {
+    const TITLE: &'static str = "pdx-explorer";
+
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for Explorer {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Pdx Explorer");
+        });
+    }
+}
+
+fn main() -> eframe::Result {
+    eframe::run_native(
+        Explorer::TITLE,
+        eframe::NativeOptions::default(),
+        Box::new(|cc| Ok(Box::new(Explorer::new(cc)))),
+    )
 }
