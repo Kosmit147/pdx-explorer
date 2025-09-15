0a7f584b54ab380eda9ee69a7f8b8847d100f673
Wojciech Opara <wwojtek660@gmail.com>
2025-09-15 19:46:39 +0200

    Add a button to select the game directory

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // Hide console window on Windows in release.
 
 use eframe::egui;
+use std::path::PathBuf;
 
 #[derive(Default)]
-struct Explorer {}
+struct Explorer {
+    game_path: Option<PathBuf>,
+}
 
 impl Explorer {
     const TITLE: &'static str = "pdx-explorer";
@@ -17,6 +20,16 @@ impl eframe::App for Explorer {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Pdx Explorer");
+
+            if ui.button("Open Game Directory").clicked()
+                && let Some(new_game_path) = rfd::FileDialog::new().pick_folder()
+            {
+                self.game_path.replace(new_game_path);
+            }
+
+            if let Some(game_path) = &self.game_path {
+                ui.label(format!("Selected path: {}", game_path.display()));
+            }
         });
     }
 }
