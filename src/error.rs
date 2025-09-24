be3a040b74dbdaaeac0f74d57352f66d99ea749c
Wojciech Opara <wwojtek660@gmail.com>
2025-09-24 22:09:23 +0200

    Parse localization files and display them in a table

---
+++ src/error.rs
@@ -0,0 +1,39 @@
+use std::{fmt, io, path::Path};
+
+#[derive(Debug, Clone)]
+pub struct Error {
+    description: String,
+}
+
+impl Error {
+    pub fn new(description: String) -> Self {
+        Self { description }
+    }
+
+    pub fn with_file_reference(path: &Path, line_number: usize, description: &str) -> Self {
+        Self::new(format!(
+            "{}:{}: {}",
+            path.display(),
+            line_number,
+            description
+        ))
+    }
+
+    pub fn description(&self) -> &String {
+        &self.description
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Self {
+            description: error.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.description)
+    }
+}
