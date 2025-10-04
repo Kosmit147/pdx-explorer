cee75709361a9963dce74bd8925bd96c764f296c
Wojciech Opara <wwojtek660@gmail.com>
2025-10-04 16:46:20 +0200

    Refactor functions which display panel contents

--- src/explorer.rs
+++ src/explorer.rs
@@ -56,42 +56,52 @@ impl Explorer {
     }
 
     fn ui(&mut self, ctx: &egui::Context) {
-        self.top_panel(ctx);
-        self.bottom_panel(ctx);
-        self.left_panel(ctx);
-        self.right_panel(ctx);
-        self.central_panel(ctx);
-    }
-
-    fn top_panel(&mut self, ctx: &egui::Context) {
         egui::TopBottomPanel::top("top panel").show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Pdx Explorer");
+            self.top_panel_content(ui);
+        });
 
-                ui.separator();
+        egui::TopBottomPanel::bottom("bottom panel").show(ctx, |ui| {
+            self.bottom_panel_content(ui);
+        });
 
-                if ui.button("Open Game/Mod Directory").clicked()
-                    && let Some(path) = rfd::FileDialog::new().pick_folder()
-                {
-                    self.set_directory(&path);
-                }
+        egui::SidePanel::left("left panel").show(ctx, |ui| {
+            self.left_panel_content(ui);
+        });
 
-                if let Some(db) = &self.database {
-                    ui.label(format!(
-                        "Selected path: {}",
-                        db.dir_tree().root_path().display()
-                    ));
-                }
-            });
+        egui::SidePanel::right("right panel").show(ctx, |ui| {
+            self.right_panel_content(ui);
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.central_panel_content(ui);
         });
     }
 
-    fn bottom_panel(&mut self, ctx: &egui::Context) {
-        egui::TopBottomPanel::bottom("bottom panel").show(ctx, |ui| {
-            if let Some(error) = &self.error {
-                ui.colored_label(egui::Color32::YELLOW, error.description());
+    fn top_panel_content(&mut self, ui: &mut egui::Ui) {
+        ui.horizontal(|ui| {
+            ui.label("Pdx Explorer");
+
+            ui.separator();
+
+            if ui.button("Open Game/Mod Directory").clicked()
+                && let Some(path) = rfd::FileDialog::new().pick_folder()
+            {
+                self.set_directory(&path);
+            }
+
+            if let Some(db) = &self.database {
+                ui.label(format!(
+                    "Selected path: {}",
+                    db.dir_tree().root_path().display()
+                ));
             }
         });
+    }
+
+    fn bottom_panel_content(&mut self, ui: &mut egui::Ui) {
+        if let Some(error) = &self.error {
+            ui.colored_label(egui::Color32::YELLOW, error.description());
+        }
     }
 
     fn dir_tree(ui: &mut egui::Ui, node: &dir::Node) {
@@ -129,67 +139,61 @@ impl Explorer {
         }
     }
 
-    fn left_panel(&mut self, ctx: &egui::Context) {
-        egui::SidePanel::left("left panel").show(ctx, |ui| {
-            ui.text_edit_singleline(&mut self.persistent_string)
-                .on_hover_text("The value in this field should persist.");
+    fn left_panel_content(&mut self, ui: &mut egui::Ui) {
+        ui.text_edit_singleline(&mut self.persistent_string)
+            .on_hover_text("The value in this field should persist.");
 
-            if let Some(db) = &self.database {
-                Self::dir_tree(ui, db.dir_tree().root());
-            }
-        });
+        if let Some(db) = &self.database {
+            Self::dir_tree(ui, db.dir_tree().root());
+        }
     }
 
-    fn right_panel(&mut self, ctx: &egui::Context) {
-        egui::SidePanel::right("right panel").show(ctx, |ui| {
-            ui.label("TODO");
-        });
+    fn right_panel_content(&mut self, ui: &mut egui::Ui) {
+        ui.label("TODO");
     }
 
-    fn central_panel(&mut self, ctx: &egui::Context) {
+    fn central_panel_content(&mut self, ui: &mut egui::Ui) {
         // todo: Language selection.
         // let selected_language = Language::English;
 
-        egui::CentralPanel::default().show(ctx, |_ui| {
-            // let Some(database) = &self.database else {
-            //     return;
-            // };
+        // let Some(database) = &self.database else {
+        //     return;
+        // };
 
-            // let Some(localization_key_map) =
-            //     database.localization_database().get(&selected_language)
-            // else {
-            //     return;
-            // };
+        // let Some(localization_key_map) =
+        //     database.localization_database().get(&selected_language)
+        // else {
+        //     return;
+        // };
 
-            // let available_height = ui.available_height();
+        // let available_height = ui.available_height();
 
-            // egui_extras::TableBuilder::new(ui)
-            //     .column(egui_extras::Column::auto())
-            //     .column(egui_extras::Column::remainder())
-            //     .striped(false)
-            //     .resizable(true)
-            //     .max_scroll_height(available_height)
-            //     .header(20.0, |mut header| {
-            //         header.col(|ui| {
-            //             ui.strong("Key");
-            //         });
-            //         header.col(|ui| {
-            //             ui.strong("Value");
-            //         });
-            //     })
-            //     .body(|mut body| {
-            //         for (key, value) in localization_key_map {
-            //             body.row(20.0, |mut row| {
-            //                 row.col(|ui| {
-            //                     ui.label(key);
-            //                 });
-            //                 row.col(|ui| {
-            //                     ui.label(value);
-            //                 });
-            //             });
-            //         }
-            //     });
-        });
+        // egui_extras::TableBuilder::new(ui)
+        //     .column(egui_extras::Column::auto())
+        //     .column(egui_extras::Column::remainder())
+        //     .striped(false)
+        //     .resizable(true)
+        //     .max_scroll_height(available_height)
+        //     .header(20.0, |mut header| {
+        //         header.col(|ui| {
+        //             ui.strong("Key");
+        //         });
+        //         header.col(|ui| {
+        //             ui.strong("Value");
+        //         });
+        //     })
+        //     .body(|mut body| {
+        //         for (key, value) in localization_key_map {
+        //             body.row(20.0, |mut row| {
+        //                 row.col(|ui| {
+        //                     ui.label(key);
+        //                 });
+        //                 row.col(|ui| {
+        //                     ui.label(value);
+        //                 });
+        //             });
+        //         }
+        //     });
     }
 }
 
